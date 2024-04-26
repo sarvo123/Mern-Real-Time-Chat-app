269ca0433f7c0dac8e934e4b38787cc295fa201c
sarvottam <kumarpurush2511@gmail.com>
2024-04-27 01:30:49 +0530

    completed the sendMessagges functionality

--- fronted/src/components/messages/MessageContainer.jsx
+++ fronted/src/components/messages/MessageContainer.jsx
@@ -1,29 +1,36 @@
 import React from "react";
 import Messages from "./Messages";
 import MessageInput from "./MessageInput";
-import {TiMessages} from "react-icons/ti";
+import { TiMessages } from "react-icons/ti";
+import useConversation from "../../zustand/useConversation";
+import { useEffect } from "react";
 
 function MessageContainer() {
   const noChatSelected = true;
+  const { selectedConversation, setSelectedConversation } = useConversation();
+
+  useEffect(() => {
+    // cleanup function (unmounts)
+    return () => setSelectedConversation(null);
+  }, [setSelectedConversation]);
 
   return (
-    <div className="md:min-w-[450px] flex flex-col">
-      {noChatSelected ? (
-        <NoChatSelected />
-      ) : (
-        <>
-          {/* Header */}
-          <div className="bg-slate-500 px-4 py-2 mb-2">
-            <span className="label-text">To:</span>{" "}
-            <span className="text-gray-900 font-bold">John doe</span>
-          </div>
-
-          <Messages />
-          <MessageInput />
-        </>
-      )}
-    </div>
-  );
+    <div className='md:min-w-[450px] flex flex-col'>
+			{!selectedConversation ? (
+				<NoChatSelected />
+			) : (
+				<>
+					{/* Header */}
+					<div className='bg-slate-500 px-4 py-2 mb-2'>
+						<span className='label-text'>To:</span>{" "}
+						<span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
+					</div>
+					<Messages />
+					<MessageInput />
+				</>
+			)}
+		</div>
+	);
 }
 
 export default MessageContainer;
--- fronted/src/components/sidebar/Sidebar.jsx
+++ fronted/src/components/sidebar/Sidebar.jsx
@@ -9,9 +9,7 @@ function Sidebar() {
       <SearchInput />
       <div className="divider" px-3></div>
       <Conversations />
-      <Conversations/>
-      <Conversations/>
-      <LogoutButton/>
+      <LogoutButton/>      
     </div>
   );
 }
